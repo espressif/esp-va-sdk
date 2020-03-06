3b2f27696a74300394f066e6d612b5b206ca2bf9
Amit Sheth <amit@espressif.com>
2020-03-06 18:49:25 +0530

    ghpages resources

---
+++ precache-manifest.db65f2e420a490135c66f9c364bc3a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ecba15681db53992b285dc60a437fc",
+    "url": "/esp-va-sdk/index.html"
+  },
+  {
+    "revision": "f921c2788294f2310d52",
+    "url": "/esp-va-sdk/static/css/main.d94ef6ff.chunk.css"
+  },
+  {
+    "revision": "5f20f10509ca5b74d023",
+    "url": "/esp-va-sdk/static/js/2.e6c8b656.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/esp-va-sdk/static/js/2.e6c8b656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f921c2788294f2310d52",
+    "url": "/esp-va-sdk/static/js/main.b8978aaf.chunk.js"
+  },
+  {
+    "revision": "6d4e26be70d7c39c73cd",
+    "url": "/esp-va-sdk/static/js/runtime-main.adcbcf45.js"
+  },
+  {
+    "revision": "84f501e3c0800cb37949e5e69424a9b7",
+    "url": "/esp-va-sdk/static/media/Espressiflogo.84f501e3.svg"
+  }
+]);
